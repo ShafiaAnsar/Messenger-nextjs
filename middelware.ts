b33aab0847f4b441619ff84a284acecee6b2418f
shafiaansar <shafiaansar142@gmail.com>
2023-11-05 18:04:39 +0500

    ..

--- middelware.ts
+++ middelware.ts
@@ -1,10 +1,11 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/', request.url))
-}
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/",
+  },
+});
+
 export const config = { 
   matcher: [
     "/conversations/:path*",
