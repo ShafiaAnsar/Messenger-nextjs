acd14ee50bbb2c7470a68d004f68ad6c3d7e1b1a
shafiaansar <shafiaansar142@gmail.com>
2023-11-05 20:54:26 +0500

    Userlist page

---
+++ app/users/components/UserBox.tsx
@@ -0,0 +1,44 @@
+'use client'
+import {useRouter} from 'next/navigation'
+import { User } from "@prisma/client"
+import { useCallback, useState } from 'react'
+import axios from 'axios'
+import Avatar from '@/app/components/Avatar'
+
+interface UserListProps{
+    data:User
+}
+const UserBox:React.FC<UserListProps> = ({data}) => {
+    const router = useRouter()
+    const [isLoading,setIsLoading] = useState(false)
+    const handleClick=useCallback(()=>{
+        setIsLoading(true)
+        axios.post('/api/conversations',{
+            userId:data.id
+        })
+        .then((data)=>{
+            router.push(`/conversation/${data.data.id}`)
+        })
+        .finally(()=>{
+            setIsLoading(false)
+        })
+    },[data,router])
+  return (
+    <div
+    onClick={handleClick}
+    className='w-full relative space-x-3 bg-white  flex items-center p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer'
+    >
+        <Avatar user={data}/>
+        <div className='min-w-0 flex-1'
+        ><div className="focus:outline-none">
+                <div className="flex justify-between items-center mb-1">
+                    <p className='test-sm font-medium text-gray-900'>{data.name}</p>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default UserBox
--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,9 +1,13 @@
+import getUsers from '../actions/getUsers'
 import Sidebar from '../components/sidebar/Sidebar'
+import UserList from './components/UserList'
 export default async function UserLayout({children}:{children:React.ReactNode}) {
+    const users = await getUsers()
     return (
         
         <Sidebar>
         <div className="h-full">
+            <UserList items={users}/>
             {children}
         </div>
         </Sidebar>
